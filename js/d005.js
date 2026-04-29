5a47d21b67e2ec8ccfbf8590df6d56f7cbf7fddb
zyin <m17837747432@163.com>
2026-04-29 10:32:17 +0800

    提交

--- js/d005.js
+++ js/d005.js
@@ -1,1 +1,13 @@
 console.log(1111111111);
+
+class A{
+    constructor(name) {
+        this.name = name
+    }
+    sayHi() {
+        console.log(this.name);
+    }
+}
+
+const a = new A('jack');
+a.sayHi()
